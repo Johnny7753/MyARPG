f178533b9a53aadfe9905850bd163f3356641c10
Johnny7753 <115707854+Johnny7753@users.noreply.github.com>
2023-05-27 16:48:49 +0800

    第三次作业

--- Source/MyARPG/TP_ThirdPerson/TP_ThirdPersonCharacter.h
+++ Source/MyARPG/TP_ThirdPerson/TP_ThirdPersonCharacter.h
@@ -43,4 +43,15 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Character")
+	void OnDamaged(float Damage, const FHitResult& HitInfo, ATP_ThirdPersonCharacter* InstigatedCharacter, AActor* DamageCauser);
+public:
+	UFUNCTION(BlueprintCallable,Category="Character")
+	virtual void HandleDamage(float Damage,const FHitResult& HitInfo,ATP_ThirdPersonCharacter* InstigatedCharacter,AActor* DamageCauser)
+	{
+		OnDamaged(Damage,HitInfo, InstigatedCharacter,DamageCauser);
+	}
+
 };
